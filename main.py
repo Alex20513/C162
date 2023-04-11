484f990c7848188b98609f8f812abb6084017bae
Alejandro Gonzalez <agonzalez13@cocc.edu>
2023-04-10 22:40:49 -0700

    Completed
    Two functions created

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 
 # Project requires TWO functions:
 # 1. rect_area (length, width) which will return the area of a rectangle
+def rect_area(length, width):
+    return length * width
+
+
 # 2. rect_solid_area (length, width, height) which will return the area of a solid rectangular object
+def rect_solid_area(length, width, height):
+    return rect_area(length, width) * height
+
 
 # The following four lines are just there to make the code work without errors until functions are added
-def rect_solid_area(x, y, z):
-    return 1
-length = 1; width = 1; height = 1
-rect_solid_area (length, width, height)
+
 
 # Request the dimension of a solid rectangular object
 length = int(input("Enter the length of the the object as in integer: "))
